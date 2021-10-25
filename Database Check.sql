ab994a5eacb4c633be3faa0102b41c7da241b6ba
Julian-felipe C Gonzales <54486338+Yamigichi@users.noreply.github.com>
2021-10-25 13:02:45 -0400

    Create Database Check.sql

---
+++ Database Check.sql
@@ -0,0 +1,9 @@
+SELECT count(name)
+FROM Track WHERE Composer = 'U2';
+
+SELECT max(Total)
+FROM Employee WHERE BillingCountry = 'Spain';
+
+
+SELECT Title
+FROM Employee WHERE LastName = 'Johnson';

cb0360416a5cf120c2973f0e1df594b298df11d8
Julian-felipe C Gonzales <54486338+Yamigichi@users.noreply.github.com>
2021-10-28 10:08:10 -0400

    Update SQL_Master_File.sql

--- SQL_Master_File.sql
+++ SQL_Master_File.sql
@@ -1,12 +1,11 @@
-//MediaType 
-
+--MediaType 
 SELECT mt.name, sum(il.Quantity) AS sales_count
 FROM InvoiceLine AS il
 INNER JOIN Track AS t ON il.TrackId = t.TrackId
 INNER JOIN MediaType AS mt ON mt.MediaTypeId = t.MediaTypeId
 GROUP BY mt.name;
 
-//Genre
+--Genre
 SELECT g.name, sum(il.Quantity) sales_count
 FROM InvoiceLine il
 INNER JOIN Track t ON il.TrackId = t.TrackId
@@ -14,7 +13,7 @@ INNER JOIN Genre g ON g.GenreId = t.GenreId
 GROUP BY g.name
 ORDER BY sum(il.Quantity) DESC;
 
-//Top Sales Agent
+--Top Sales Agent
 SELECT Employee.hiredate Hire_Date,Employee.firstname,
 Employee.lastname LastName, COUNT(Invoice.invoiceid) Total_Sales,
 ROUND(SUM(Invoice.total), 2) Value_of_Sale
@@ -23,7 +22,7 @@ JOIN customer ON customer.supportrepid = employee.employeeid
 JOIN invoice ON invoice.customerid = customer.customerid
 WHERE title = 'Sales Support Agent' GROUP BY 1
   
-//Top Customer
+--Top Customer
 SELECT Customer.FirstName,Customer.LastName, Customer.Country, round(SUM(Invoice.Total)) AS Total_Amount
 FROM Invoice
 JOIN Customer
